2ef55cb5ac673503f6be7b108725a68e4ec4a25b
Lauren Yatar <laurenyatar@Laurens-MacBook-Pro.local>
2017-02-22 23:29:22 -0800

    Assignment 4 Files
    …

---
+++ sandbox/YATAR Lauren/Assignment 4/script2.js
@@ -0,0 +1,134 @@
+
+
+function onDataLoaded(dObj) {
+    console.log("data is loaded, i'm ready to go!");
+    console.log(dObj);
+
+
+    var barPadding = 1;  // space between the bars
+
+
+    // add a board (an SVG) to the canvas. Uses a DY Utility function to easily add an svg and calculate inner and outer dimensions. Returns an object of {g (an SVG), bDims (the board dimensions), dDims (the draw dimensions)} Each dimensions have width, height, xRange, and yRange members.
+    // the board SVG contains a "group" to handle the margin effectively. This inner group works as a sort of inner SVG that contains an origin translated by the x and y offsets. Think of the new 0,0 point of your working SVG as the inner drawing origin of this group. Dimensions are accessible via board.dDims (drawing dimensions) and board.bDims (board dimensions).
+    board = dY.graph.addBoard("#dy-canvas",{inWidth:300, inHeight: 250 ,margin:40});
+    console.log(board);
+
+    // We define yValue as a function that, given a data item, returns the proper value for this axis
+    var yValue = function(d) { return d.mean; }; // data -> value
+
+    // We define yScale as a D3 scale function that, given a value in one domain (the domain of values taken from our data), returns a coordinate on the canvas (a location within the drawing dimensions of our board).
+    // Recall that x-coordinates move from left to right and y-coordinates move from top to bottom.
+    var yScale = d3.scale.linear()  // value -> display
+        .domain([0,24])
+        .range((board.dDims.yRange));
+
+    var xScale = d3.scale.linear()
+        .domain([0,20])
+        .range((board.dDims.xRange));
+
+    // So scle will visually show -20 to 20
+    var xScale2 = d3.scale.linear()
+        .domain([-20,20])
+        .range((board.dDims.xRange));
+
+
+
+    // We define yMap as a combonation of the two functions defined above.
+    // A function that, given a data item, returns a coordinate on the canvas
+    var yMap = function(d) { return yScale(yValue(d));}; // data -> display
+    var xMap = function(d) { return xScale(yValue(d));}; // data -> display
+
+    // D3 will draw an axis to the canvas for us - ticks, labels and all!
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .ticks(24)
+        // .tickValues([0,0,30])
+        .orient("left");
+
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale2)
+        .ticks(289/12)
+        .orient("bottom");
+
+
+
+    // draw the y-axis
+    board.g.append("g") // appends a new group to the board
+        .attr({
+            class: "y axis",
+            transform: "translate(-10,0)" // moves this group a bit to the left
+        })
+        .call(yAxis);
+
+    //draw the x-axis
+
+        board.g.append("g") // appends a new group to the board
+            .attr({
+                class: "x axis",
+                transform: "translate(0,252)" // moves this group a bit down
+            })
+            .call(xAxis);
+
+    // draw the bars
+
+    board.g.selectAll("rect")
+        .data(dObj)
+        .enter()
+        .append("rect")
+        .attr({
+            class: "bar",
+            x: function(d) { if(d.mean < 0) { return (board.dDims.width / 2) - Math.abs((xMap(d)/2));}
+            else return (board.dDims.width / 2);},
+            y: function(d, i) { return i * (board.dDims.height / dObj.length); },
+            height: '.75'*(board.dDims.width / dObj.length - barPadding),
+            width: function(d) { return Math.abs((xMap(d)/2));}
+            })
+        .style({
+            "opacity": function(d) {if(d.mean <0) {return '.25';}
+            else return '.75'; },
+            "fill": "tomato",
+          })
+        //Hovering Effects
+        .on("mouseout", function(d,i) {
+            d3.select(this).transition()
+            .style("fill", "gray");})
+        .on("mouseover", function(d,i) {
+            d3.select(this).transition()
+            .style("fill", "tomato");
+          });
+
+        // For side numbers
+
+         board.g.selectAll("text")
+            .data(dObj)
+            .text(function(d) {
+                 return d.mean;
+            })
+            .attr("x", 320)
+            .attr("y", function(d, i) { return (board.dDims.height / dObj.length) - 5;})
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "8px")
+            .attr("fill", "red")
+            .on("mouseout", function(d,i) {
+                d3.select(this).transition()
+                .ease("elasticout")
+                .delay("100")
+                .duration("200")
+                .attr("x", "320");})
+            .on("mouseover", function(d,i) {
+                d3.select(this).transition()
+                .ease("elasticout")
+                .delay("100")
+                .duration("200")
+                .attr("x", 320/2);});
+
+
+
+
+
+      //trolololol
+      d3.selectAll("rect").transition().delay(1000).style("fill", "gray");
+      d3.select('svg').append('text').attr('x', 20).attr('y', 25).text('Average Degrees from Comfort, Averaged Hourly').style('font-family', 'Geneva').style('font-size', 11);
+
+}
