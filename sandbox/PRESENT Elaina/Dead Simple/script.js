50a57d8fa3b9fe1ca7b6e667683d47aeabf69d0f
Kyle Steinfeld <ksteinfe@gmail.com>
2017-02-27 12:42:27 -0800

    work!

--- sandbox/PRESENT Elaina/Dead Simple/script.js
+++ sandbox/PRESENT Elaina/Dead Simple/script.js
@@ -8,27 +8,26 @@ function onDataLoaded(dObj) {
     // the board SVG contains a "group" to handle the margin effectively. This inner group works as a sort of inner SVG that contains an origin translated by the x and y offsets. Think of the new 0,0 point of your working SVG as the inner drawing origin of this group. Dimensions are accessible via board.dDims (drawing dimensions) and board.bDims (board dimensions).   
     board = dY.graph.addBoard("#dy-canvas",{inWidth: 200, inHeight:200, margin:40});
     //console.log(board);
-      
-
+    
+    
     // setup x
-    var xValue = function(d) { return d.valueOf("DryBulbTemp"); }; // data -> value
+    var xValue = function(d,i) { return i; }; // data -> value // i is the index of the data point
     var xScale = d3.scale.linear()  // value -> display
-        .domain([-20,40])
+        .domain([0,23])
         .range((board.dDims.xRange));
-    var xMap = function(d) { return xScale(xValue(d));}; // data -> display
+    var xMap = function(d,i) { return xScale(xValue(d,i));}; // data -> display
     var xAxis = d3.svg.axis()
         .scale(xScale);
       
     // setup y
-    var yValue = function(d) { return d.valueOf("RelHumid"); }; // data -> value
     var yScale = d3.scale.linear() // value -> display
-        .domain([100,0])
+        .domain([90,70])
         .range((board.dDims.yRange)); 
-    var yMap = function(d) { return yScale(yValue(d));}; // data -> display
     var yAxis = d3.svg.axis()
         .scale(yScale)
         .orient("left");
-      
+
+
     // x-axis
     board.g.append("g")
         .attr({
@@ -44,18 +43,28 @@ function onDataLoaded(dObj) {
             transform: "translate(-10,0)"
         })
         .call(yAxis)
-      
-    // draw dots
-    board.g.selectAll(".dot")
-        .data(dObj.ticks)
-        .enter().append("circle")
-            .attr({
-                class: "dot",
-                //class: function(d){ if (d.data.EPW.DewPt > 0) {return "dot high-dew"} else {return "dot low-dew"}  },
-                r: 1.5,
-                cx: function(d) { return xMap(d);} ,
-                cy: function(d) { return yMap(d);} ,
-            })
-      
+        
+    for (var s in dObj.schema){
+        console.log(s);
+    
+        var yValue = function(d) { return d.data[s].OperativeTemperature; }; // data -> value
+        
+        // draw dots
+        board.g.append("g")
+            .attr("class",s)
+            .style("fill", "blue")
+            .selectAll(".dot")
+                .data(dObj.ticks)
+                .enter().append("circle")
+                    .attr({
+                        class: "dot",
+                        //class: function(d){ if (d.data.EPW.DewPt > 0) {return "dot high-dew"} else {return "dot low-dew"}  },
+                        r: 1.5,
+                        cx: function(d,i) { return xMap(d,i);} ,
+                        cy: function(d) { return yScale(yValue(d)); } 
+                    })
+            
+    }
+            
 }
 
