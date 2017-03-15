f0cb1e11c6f6a8d1e10fcc74dba0eb2c9e5af5a4
Kyle Steinfeld <ksteinfe@gmail.com>
2017-03-15 12:57:19 -0700

    rania

--- sandbox/STEINFELD Kyle/170228a ground temp/script.js
+++ sandbox/STEINFELD Kyle/170228a ground temp/script.js
@@ -12,6 +12,28 @@ function onDataLoaded(dObj) {
     
     // summary information for each day of the year
     var dSum = dObj.dailySummary();
+    //console.log(dSum);
+    
+    
+    var areaData = [];
+    for (var d in dSum){        
+        dobj = {}
+        dobj.airTemp = dSum[d].averageOf("DryBulbTemp");
+        dobj.monthOfYear = dSum[d].ts.monthOfYear();
+        dobj.dayOfYear = d;
+        
+        var firstGroundTemp = dObj.epwhead.ground[0].monthlyTemperature[dobj.monthOfYear];
+        var secondGroundTemp = dObj.epwhead.ground[1].monthlyTemperature[dobj.monthOfYear];
+
+        dobj.firstgroundTemp = firstGroundTemp;
+        dobj.secondGroundTemp = secondGroundTemp;
+        //dobj.thirdGroundTemp = thirdGroundTemp;
+        dobj.isGoodForFogFirst = dobj.airTemp < dobj.firstgroundTemp;
+        dobj.isGoodForFogSecond = dobj.airTemp < dobj.secondGroundTemp;
+        areaData.push(dobj);
+    }
+    console.log(areaData);
+
     
     // add a board (an SVG) to the canvas. Uses a DY Utility function to easily add an svg and calculate inner and outer dimensions. Returns an object of {g (an SVG), bDims (the board dimensions), dDims (the draw dimensions)} Each dimensions have width, height, xRange, and yRange members.
     // the board SVG contains a "group" to handle the margin effectively. This inner group works as a sort of inner SVG that contains an origin translated by the x and y offsets. Think of the new 0,0 point of your working SVG as the inner drawing origin of this group. Dimensions are accessible via board.dDims (drawing dimensions) and board.bDims (board dimensions).   
@@ -27,6 +49,10 @@ function onDataLoaded(dObj) {
     var xScaleHr = d3.scale.linear()  // value -> display
         .domain([0,8760])
         .range(board.dDims.xRange);     
+        
+    var xScaleDay = d3.scale.linear()  // value -> display
+        .domain([0,364])
+        .range(board.dDims.xRange);            
         
     var xScaleTime = d3.time.scale()
         .domain([new Date(dY.dt.year, 0, 1), new Date(dY.dt.year, 11, 31)])
@@ -58,6 +84,12 @@ function onDataLoaded(dObj) {
     var yMapLow = function(d) { return yScale(d.q1Of("DryBulbTemp")) }; // data -> display
         
     
+    // Setup Color Scalse
+    var cScale = d3.scale.linear()
+    .domain([0,6])
+    .interpolate(d3.interpolate)
+    .range([d3.rgb("#bbb"), d3.rgb('#000')]);
+    
     // Setup Line and Area Functions
         
     var lineFuncGroundTemp = d3.svg.line()
@@ -66,23 +98,25 @@ function onDataLoaded(dObj) {
         .interpolate('step-before');
       
     var lineFunctionDryBulbAvg = d3.svg.line()
-        .x( function(d){ return xScaleHr(d.hourOfYear()); } )
+        .x( function(d){ return xScaleDay(d.dayOfYear()); } )
         .y(yMapAvg)
         .interpolate("linear");
         
-    var areaFunctionDryBulbHiLo = d3.svg.area()
-        .x( function(d){ return xScaleHr(d.hourOfYear()); } )
-        .y0(yMapLow)
-        .y1(yMapHigh)
+    var areaFunctionFogFirst = d3.svg.area()
+        .x( function(d){ return xScaleDay(d.dayOfYear); } )
+        .y0( function(d){ return yScale(d.firstgroundTemp)} )
+        .y1( function(d){ return yScale(d.airTemp)} )
+        .defined( function(d){ return d.isGoodForFogFirst } ) 
         .interpolate("linear");   
-      
+        
+    var areaFunctionFogSecond = d3.svg.area()
+        .x( function(d){ return xScaleDay(d.dayOfYear); } )
+        .y0( function(d){ return yScale(d.secondgroundTemp)} )
+        .y1( function(d){ return yScale(d.airTemp)} )
+        .defined( function(d){ return d.isGoodForFogSecond } ) 
+        .interpolate("linear");         
       
     // draw x-axis
-    board.g.append("g")
-        .attr({
-            class: "x axis"
-        })
-        .call(xAxisMnth)
         
     board.g.append("g")
         .attr({
@@ -101,23 +135,31 @@ function onDataLoaded(dObj) {
         })
         .call(yAxis)
     
-    // draw hi/low dry-bulb temperature area 
+    // draw area for fog potential 
     board.g.append("path")
-        .attr("d", areaFunctionDryBulbHiLo(dSum))
-        .attr("class", "temp-area")     
-      
+        .attr("d", areaFunctionFogFirst(areaData))
+        .attr("class", "fog-area first")     
+        
+    board.g.append("path")
+        .attr("d", areaFunctionFogSecond(areaData))
+        .attr("class", "fog-area second")   
+
+        
     // draw average dry-bulb temperature 
     board.g.append("path")
         .attr("d", lineFunctionDryBulbAvg(dSum))
         .attr("class", "temp-average")   
         
     // draw some number of ground temperature step lines
+    /*
     board.g.append("g")
         .attr("class", "groundline")
         .selectAll("path")
             .data(dObj.epwhead.ground)
             .enter().append("path")
+                .attr("stroke", function(d){ return cScale(d.depth) ; })
                 .datum(function(d){ return d.monthlyTemperature; })
-                .attr( "d", lineFuncGroundTemp );          
+                .attr( "d", lineFuncGroundTemp )     
+                */
 }
 
