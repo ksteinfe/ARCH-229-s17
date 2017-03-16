1ee09d76b56235f6b788228726c7980a36ace40a
jmwoolley <jmwoolley@gmail.com>
2017-03-16 10:05:29 -0700

    Deleting Old Versions in order to sync properly
    attempting to get GitHub to sync properly

--- sandbox/WOOLLEY Jonathan/drafts/copied from block sections/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-
-export default {
-  entry: "d3.js",
-  plugins: [nodeResolve({jsnext: true})],
-  moduleId: "d3",
-  moduleName: "d3",
-  format: "umd"
-};
